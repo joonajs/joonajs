62c76feff4e56da20c25fdabc804a05956b59737
joonajs <joonajs@proton.me>
2025-01-19 23:26:05 +0200

    layout :)

--- src/App.jsx
+++ src/App.jsx
@@ -23,76 +23,78 @@ useEffect(() => {
 
 
   return (
-      <div className="font-sans font-medium text-black bg-[#fcfcfce6]">
-          <Navigation />
-          <main className="mt-20 p-6 min-w-full left-1/2 -transform-x-1/2 mx-auto lg:grid lg:grid-cols-6 lg:gap-6 max-w-[1920px] overflow-hidden">
-              <div className="lg:col-span-4 lg:pr-10 px-5 py-5 rounded-3xl border bg-white">
-                  <section id="about" className="mb-4 max-w-4xl ">
-                      <h1 className="text-2xl rounded-3xl w-fit font-bold mb-2">About Me</h1>
-                      <p className=" text-lg leading-tight">
-                          Hey there! My name is Joona and I'm a 3rd-year Information Technology student at Vaasa University of Applied Sciences, majoring in software development. I'm interested in everything technology-related, with a passion for UI/UX and design aswell. You should also check out my
-                          <a target="_blank" href="https://www.github.com/joonajs" className="text-blue-500 hover:underline"> GitHub </a>
-                          where I have some of my personal projects, as well as my
-                          <a target="_blank" href="https://www.gitlab.com/joonajs" className="text-blue-500 hover:underline"> GitLab </a>
-                          where I have some of my school projects.
-                      </p>
-                  </section>
-                  <section id="projects" className="mb-4 max-w-3xl text-lg leading-tight">
-                      <h2 className="text-2xl rounded-3xl w-fit font-bold mb-2">Projects</h2>
-                      <p className="mb-2">Small list of some projects I have done, will be updating this in the future.</p>
-                      <div className="space-y-2">
-                          <div>
-                              <p><strong>Magic Bin</strong> was made as the final project for my school course in React development. In it, a user can type a 1000 character text-bin to be stored and you get a token you can share to other users to retrieve.</p>
-                              <a href="https://w81-magicbin.web.app/" className="text-blue-500 hover:underline">View Project</a>
-                          </div>
-                          <div>
-                              <p><strong>Sorter</strong> is a straightforward web app that helps users organize text documents by sorting their content alphabetically.</p>
-                              <a href="https://github.com/joonajs/sorter/" className="text-blue-500 hover:underline">View Project</a>
-                          </div>
-                          <div>
-                              <p><strong>XTemps</strong> is a temporary file server built for quick file sharing. It combines a Flask backend with a React frontend, allowing users to spin up a local server for ephemeral file transfers.</p>
-                              <a href="https://github.com/joonajs/xtfs" className="text-blue-500 hover:underline">View Project</a>
-                          </div>
-                          <div>
-                              <p><strong>HRMA</strong> is a Hotel Room Management Application, in which users can log in or sign up and book hotel rooms, manage them, etc. Made for DevOps -course as a Final Project, albeit the focus was on CI/CD and Docker.</p>
-                              <a href="https://gitlab.com/joonajs/devops-finalproject" className="text-blue-500 hover:underline">View Project</a>
-                          </div>
-                      </div>
-                  </section>
-                  <section id="design" className="mb-4 max-w-3xl ">
-            <h2 className="text-2xl rounded-3xl w-fit font-bold mb-2">Design</h2>
-            <p className="text-lg leading-tight">
-           As much as I love developing software and programming, I also have a passion for design. I have experience in creating user interfaces and user experiences for web applications, mobile applications, and websites. I have a strong understanding of design principles and I always strive to create visually appealing and user-friendly interfaces. You can view my work on <a href="https://www.behance.net/joonajs" className="text-blue-500 hover:underline">Behance</a> as well as <a href="https://www.x.com/joona_js" className="text-blue-500 hover:underline">X</a>.
+    <div className="font-sans font-medium text-black bg-[#fcfcfce6]">
+      <Navigation />
+      <main className="mt-20 p-6 min-w-full left-1/2 -transform-x-1/2 mx-auto max-w-[1920px] overflow-hidden">
+        <div className="p-5 border rounded-3xl bg-white">
+          <div className="grid lg:grid-cols-3 gap-6">
+            <div className="lg:col-span-2 px-5 py-5 rounded-3xl">
+              <section id="about" className="mb-4 max-w-4xl">
+                <h1 className="text-2xl rounded-3xl w-fit font-bold mb-2 bg-neutral-100 px-3 text-black">About Me</h1>
+                <p className="text-lg leading-tight">
+                  Hey there! My name is Joona and I'm a 3rd-year Information Technology student at Vaasa University of Applied Sciences, majoring in software development. I'm interested in everything technology-related, with a passion for UI/UX and design aswell. You should also check out my
+                  <a target="_blank" href="https://www.github.com/joonajs" className="text-blue-500 hover:underline"> GitHub </a>
+                  where I have some of my personal projects, as well as my
+                  <a target="_blank" href="https://www.gitlab.com/joonajs" className="text-blue-500 hover:underline"> GitLab </a>
+                  where I have some of my school projects.
+                </p>
+              </section>
+              <section id="projects" className="mb-4 max-w-3xl text-lg leading-tight">
+                <h2 className="text-2xl rounded-3xl w-fit font-bold mb-2 bg-neutral-100 px-3 text-black">Projects</h2>
+                <p className="mb-2">Small list of some projects I have done, will be updating this in the future.</p>
+                <div className="space-y-2">
+                  <div>
+                    <p><strong>Magic Bin</strong> was made as the final project for my school course in React development. In it, a user can type a 1000 character text-bin to be stored and you get a token you can share to other users to retrieve.</p>
+                    <a href="https://w81-magicbin.web.app/" className="text-blue-500 hover:underline">View Project</a>
+                  </div>
+                  <div>
+                    <p><strong>Sorter</strong> is a straightforward web app that helps users organize text documents by sorting their content alphabetically.</p>
+                    <a href="https://github.com/joonajs/sorter/" className="text-blue-500 hover:underline">View Project</a>
+                  </div>
+                  <div>
+                    <p><strong>XTemps</strong> is a temporary file server built for quick file sharing. It combines a Flask backend with a React frontend, allowing users to spin up a local server for ephemeral file transfers.</p>
+                    <a href="https://github.com/joonajs/xtfs" className="text-blue-500 hover:underline">View Project</a>
+                  </div>
+                  <div>
+                    <p><strong>HRMA</strong> is a Hotel Room Management Application, in which users can log in or sign up and book hotel rooms, manage them, etc. Made for DevOps -course as a Final Project, albeit the focus was on CI/CD and Docker.</p>
+                    <a href="https://gitlab.com/joonajs/devops-finalproject" className="text-blue-500 hover:underline">View Project</a>
+                  </div>
+                </div>
+              </section>
+              <section id="design" className="mb-4 max-w-3xl">
+                <h2 className="text-2xl rounded-3xl w-fit font-bold mb-2 bg-neutral-100 px-3 text-black">Design</h2>
+                <p className="text-lg leading-tight">
+                  As much as I love developing software and programming, I also have a passion for design. I have experience in creating user interfaces and user experiences for web applications, mobile applications, and websites. I have a strong understanding of design principles and I always strive to create visually appealing and user-friendly interfaces. You can view my work on <a href="https://www.behance.net/joonajs" className="text-blue-500 hover:underline">Behance</a> as well as <a href="https://www.x.com/joona_js" className="text-blue-500 hover:underline">X</a>.
+                </p>
+              </section>
+              <section id="contact" className="lg:mb-0 mb-10 text-lg leading-tight">
+                <h2 className="text-2xl rounded-3xl w-fit font-bold mb-2 bg-neutral-100 px-3 text-black">Contact</h2>
+                <p>
+                  Feel free to reach out to me via email at
+                  <a href="joonajs@proton.me" className="text-blue-500 hover:underline pl-1">
+                    joonajs@proton.me
+                  </a>.
+                </p>
+              </section>
+            </div>
 
-            </p>
-              
-
-          </section>
-          <section id="contact" className="lg:mb-0 mb-10 text-lg leading-tight">
-                      <h2 className="text-2xl rounded-3xl w-fit font-bold mb-2  ">Contact</h2>
-                      <p>
-                          Feel free to reach out to me via email at 
-                          <a href="joonajs@proton.me" className="text-blue-500 hover:underline pl-1">
-                              joonajs@proton.me
-                          </a>.
-                      </p>
-                  </section>
+            {/* Image section */}
+            <div className="lg:col-span-1 space-y-4">
+              <div className="relative">
+                <img
+                  src="demoapp.svg"
+                  alt="A design for Mobile Application"
+                  className="rounded-3xl border object-cover opacity-50 hover:opacity-100 hover:cursor-pointer transition-all"
+                />
+                <p className="text-xs text-left max-w-lg text-neutral-400 mt-2">
+                  A design prototype for a payment mobile application made in an evening with Figma.
+                </p>
               </div>
-         {/* Image section */}
-         <div className="lg:col-span-2 space-y-4 lg:mt-0 mt-2 min-w-full">
-            <div className="relative">
-              <img
-                src="demoapp.svg"
-                alt="A design for Mobile Application"
-                className="rounded-3xl border object-cover opacity-50 hover:opacity-100 hover:cursor-pointer transition-all"
-              />
-              <p className="text-xs text-left max-w-lg text-neutral-400 mt-2">
-                A design prototype for a payment mobile application made in an evening with Figma.
-              </p>
             </div>
           </div>
+        </div>
+      </main>
 
-          </main>
           <footer className=" m-6 rounded-3xl h-fit bg-white border mt-0 px-5 py-5">
                 <p className="text-sm">
                     This website is still hugely a work in progress, so please excuse the mess. I'm currently working on adding more content and improving the design. If you have any feedback or suggestions, feel free to reach out to me via email. I'm trying to add more projects and design work to the site, so stay tuned for updates!
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -12,28 +12,28 @@ const Navigation = () => {
 
   return (
     <nav className="flex justify-center items-center border mx-auto lg:fixed lg:right-10 lg:left-auto lg:translate-x-0 left-1/2 -translate-x-1/2 py-1 px-1 fixed top-0 bg-white m-5 w-fit rounded-full bg-opacity-90 backdrop-blur-3xl z-10">
-   <ul className="flex space-x-5 md:text-2xl text-xl font-semibold group">
+   <ul className="flex space-x-5 lg:text-4xl text-xl font-semibold group">
         <li
           onClick={() => scrollToSection('about')}
-          className="hover:bg-blue-500 hover:text-white px-2 md:px-3 md:py-1 rounded-full cursor-pointer transition-all duration-200  md:group-hover:text-3xl"
+          className="hover:bg-blue-500 hover:text-white px-2 lg:px-3 lg:py-1 rounded-full cursor-pointer transition-all duration-200  lg:group-hover:text-[2.85rem] lg:group-hover:px-5 lg:group-hover:py-2"
         >
           About
         </li>
         <li
           onClick={() => scrollToSection('projects')}
-          className="hover:bg-blue-500 hover:text-white px-2 md:px-3 md:py-1 rounded-full cursor-pointer transition-all duration-200 md:group-hover:text-3xl"
+          className="hover:bg-blue-500 hover:text-white px-2 lg:px-3 lg:py-1 rounded-full cursor-pointer transition-all duration-200 lg:group-hover:text-[2.85rem] lg:group-hover:px-5 lg:group-hover:py-1"
         >
           Projects
         </li>
         <li
           onClick={() => scrollToSection('design')}
-          className="hover:bg-blue-500 hover:text-white px-2 md:px-3 md:py-1 rounded-full cursor-pointer transition-all duration-200 md:group-hover:text-3xl"
+          className="hover:bg-blue-500 hover:text-white px-2 lg:px-3 lg:py-1 rounded-full cursor-pointer transition-all duration-200 lg:group-hover:text-[2.85rem] lg:group-hover:px-5 lg:group-hover:py-1"
         >
           Design
         </li>
         <li
           onClick={() => scrollToSection('contact')}
-          className="hover:bg-blue-500 hover:text-white px-2 md:px-3 md:py-1 rounded-full cursor-pointer transition-all duration-200 md:group-hover:text-3xl"
+          className="hover:bg-blue-500 hover:text-white px-2 lg:px-3 lg:py-1 rounded-full cursor-pointer transition-all duration-200 lg:group-hover:text-[2.85rem] lg:group-hover:px-5 lg:group-hover:py-1"
         >
           Contact
         </li>
